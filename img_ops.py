8df2da9b5ecdbd90c309139b44f46a94f76c04d1
Ted Xie <ted.xie@virginia.edu>
2017-11-01 20:35:45 -0400

    skeleton code mostly done

--- img_ops.py
+++ img_ops.py
@@ -1,7 +1,47 @@
+import numpy as np
+
 def resize(img, amount):
-    return img
+    img_tmp = np.copy(img)
+    MAX_X = np.shape(img)[0]
+    MAX_Y = np.shape(img)[1]
 
-def min_cut(img):
+    if amount == 0: return img_tmp
+    if amount < 0:
+        amount = amount * -1
+        for i in range(amount):
+            # find the min cut path
+            path = min_cut(img_tmp)
+            # stitch 1 pixel at a time
+            for node in path:
+                nodex = node[0]
+                nodey = node[1]
+                # copy the row contents before and after the path node
+                for x in range(0, nodex):
+                    img_tmp[x, nodey] = img[x, nodey]
+                for x in range(nodex+1, MAX_X):
+                    img_tmp[x-1, nodey] = img[x, nodey]
+    return img_tmp[0:MAX_X-amount, 0:MAX_Y]
+
+def dijkstra(img, start_node, end_node, MAX_X, MAX_Y):
     min_cost = float("inf")
-    min_cut = None
+    min_path = []
 
+
+def min_cut(img, MAX_X, MAX_Y):
+#    min_cost = float("inf")
+#    min_cut = None
+#    
+#    for x in range(MAX_X):
+#        start_node = (x, 0)
+#        for xx in range(MAX_X):
+#            end_node = (xx, MAX_Y-1)
+#            cost_i, cut_i = dijkstra(img, start_node, end_node, MAX_X, MAX_Y)
+#            if cost_i < min_cost:
+#                min_cost = cost_i
+#                min_cut = cut_i
+    min_cost = random.random()
+    min_cut = []
+    for y in range(0, MAX_Y):
+        x = random.randrange(0, MAX_X)
+        min_cut.append(x, y)
+    return (min_cost, min_cut) 
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import argparse
-
+# scipy imports
 from scipy import ndimage
 from scipy import misc
-
+# numpy imports
+import numpy as np
+# matplotlib imports
 import matplotlib.pyplot as plt
-
+# skimage imports
+from skimage import color
+from skimage import io
+# local imports
 from img_ops import resize
 
 # Argument parser
@@ -13,27 +18,50 @@ parser.add_argument("-i", "--img", required=True, help="Input image name")
 parser.add_argument("-o", "--output", required=False, help="(Optional) Output image name, default to out_<image_name")
 parser.add_argument("-nx", "--xpixels", required=True, help="Number of pixels to reduce in the X-dimension")
 parser.add_argument("-ny", "--ypixels", required=True, help="Number of pixels to reduce in the Y-dimension")
+parser.add_argument("-s", "--show", required=False, help="show the output image compared to the original", action="store_true")
 
 args = parser.parse_args()
 
-img = misc.imread(args.img)
+img = color.rgb2gray(io.imread(args.img, mode="RGB"))
 
 # Initially set outfile path to be "out_"+<image_name>
 # If output path is specified, use that instead
 output_file_path = "out_" + args.img
-if (args.output):
+if args.output:
     output_file_path = args.output
 
+# Cast command line args to string
+nx = int(args.xpixels)
+ny = int(args.ypixels)
+
 # First resize by X dimension
-result = resize(img, args.xpixels)
+result = resize(img, nx)
 
 # Then resize by Y dimension
 # This can be done by rotating by 90 degrees and repeating the resize algo
 result = ndimage.rotate(result, 90)
-result = resize(result, args.ypixels)
+result = resize(result, ny)
 
 # Rotate back to original orientation
 result = ndimage.rotate(result, 270)
 
+orig_shape = np.shape(img)
+new_shape = np.shape(result)
+print("Original dimensions: xdim=%d, ydim=%d" % (orig_shape[0], orig_shape[1]))
+print("New dimensions: xdim=%d, ydim=%d" % (new_shape[0], new_shape[1]))
+
 # Write out result
 misc.imsave(output_file_path, result)
+
+# Show the result if specified
+if args.show:
+    plt.subplot(2,1,1)
+    plt.imshow(img, cmap="gray")
+    plt.title("Original image")
+
+    plt.subplot(2,1,2)
+    plt.imshow(result, cmap="gray")
+    plt.title("Resulting image")
+    plt.show()
+
+    plt.axis("off") # don't show X and Y axis
